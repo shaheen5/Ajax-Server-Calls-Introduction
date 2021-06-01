68514a7d504c90544f1746df70b2bbbb2d76a6e6
shaheen5 <miyashaheen1995@gmail.com>
2021-06-01 22:43:11 +0530

    Added : Code To Demonstrate Asynchronous Nature Of Javascript

--- timer.js
+++ timer.js
@@ -1,1 +1,14 @@
-console.log("Welcome to AJAX - Client Server Interaction !");
+//program to demonstrate async nature of javascript
+function showTime() {
+	const date = new Date();
+	return date.getHours() + "Hrs:" +date.getMinutes()+"Mins:"+date.getSeconds()+"Secs";
+
+}
+
+function showSessionExpire(){
+console.log("Activity-B : Your session expired at :"+showTime());
+}
+
+console.log("Activity-A : Triggerring Activity-B at :"+showTime());
+setTimeout(showSessionExpire,5000);
+console.log("Activity-A : Triggereg Acitvity-B at "+showTime()+"will execute after 5 seconds");
